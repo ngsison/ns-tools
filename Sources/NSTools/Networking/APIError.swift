431f3db5a35a4810c08ed591fb1d9d2530ef3b35
Nathaniel Brion Sison <ngsison@outlook.com>
2025-04-04 03:24:18 +0800

    Add responseMapping error

--- Sources/NSTools/Networking/APIError.swift
+++ Sources/NSTools/Networking/APIError.swift
@@ -1,9 +1,10 @@
 public enum APIError: Error {
     case invalidURL
     case noResponse
+    case underlying(_ error: Error)
     case encodableMapping(_ error: Error)
     case decodableMapping(_ error: Error)
-    case underlying(_ error: Error)
+    case responseMapping(_ result: APIResult)
     case unauthorized(_ result: APIResult)
     case statusCode(_ result: APIResult)
     case unknown(_ result: APIResult)
