f22da73fdf0f794e4d9a1ca81563ac25bf2ecfa9
Nathaniel Brion Sison <ngsison@outlook.com>
2025-04-04 02:37:47 +0800

    Make headers, body, and urlParameters optional

--- Sources/NSTools/Networking/API.swift
+++ Sources/NSTools/Networking/API.swift
@@ -4,9 +4,9 @@ public protocol API {
     var baseURL: String { get }
     var path: String { get }
     var method: APIMethod { get }
-    var headers: [String: String] { get }
-    var body: APIBody { get }
-    var urlParameters: [String: String?] { get }
+    var headers: [String: String]? { get }
+    var body: APIBody? { get }
+    var urlParameters: [String: String?]? { get }
 }
 
 extension API {
@@ -24,7 +24,7 @@ extension API {
     private func buildUrl() throws -> URL {
         var urlComponents = URLComponents(string: baseURL)
         urlComponents?.path = path
-        urlComponents?.queryItems = urlParameters.compactMap { key, value in
+        urlComponents?.queryItems = urlParameters?.compactMap { key, value in
             URLQueryItem(name: key, value: value)
         }
         
@@ -37,7 +37,7 @@ extension API {
     
     private func buildHeaders() -> [String: String] {
         var allHeaders: [String: String] = ["Content-Type": "application/json"]
-        headers.forEach { allHeaders[$0.key] = $0.value }
+        headers?.forEach { allHeaders[$0.key] = $0.value }
         return allHeaders
     }
     
--- Sources/NSTools/Networking/APIBody.swift
+++ Sources/NSTools/Networking/APIBody.swift
@@ -1,5 +1,4 @@
 public enum APIBody {
     case encodable(Encodable)
     case dictionary([String: Any])
-    case none
 }
