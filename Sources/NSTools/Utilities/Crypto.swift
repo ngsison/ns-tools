c3cb9c78e7dc8acb217181d0a6ae0697e96b1614
Nathaniel Brion Sison <ngsison@outlook.com>
2025-04-19 02:46:04 +0800

    Add Crypto

---
+++ Sources/NSTools/Utilities/Crypto.swift
@@ -0,0 +1,34 @@
+import Foundation
+import CryptoKit
+
+public enum Crypto {
+    public static func randomNonceString(length: Int = 32) -> String {
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms = (0..<16).map { _ in UInt8.random(in: 0...255) }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random) % charset.count])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    public static func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashed = SHA256.hash(data: inputData)
+        return hashed.compactMap { String(format: "%02x", $0) }.joined()
+    }
+}
