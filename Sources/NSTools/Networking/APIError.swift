e62b72f899dd72e79de56ccbc2421d62cd240656
Nathaniel Brion Sison <ngsison@outlook.com>
2025-04-24 01:24:48 +0800

    Add errorDescription

--- Sources/NSTools/Networking/APIError.swift
+++ Sources/NSTools/Networking/APIError.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public enum APIError: Error {
     case invalidURL
     case noResponse
@@ -10,4 +12,33 @@ public enum APIError: Error {
     case unauthorized(_ result: APIResult)
     case statusCode(_ result: APIResult)
     case unknown(_ result: APIResult)
+}
+
+extension APIError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .noResponse:
+            return "No response"
+        case .noData:
+            return "No data"
+        case .notImplemented:
+            return "Not implemented"
+        case .underlying(let error):
+            return "Underlying error"
+        case .encodableMapping(let error):
+            return "Encoding failed"
+        case .decodableMapping(let error):
+            return "Decoding failed"
+        case .responseMapping(let result):
+            return "Invalid response format"
+        case .unauthorized:
+            return "Unauthorized"
+        case .statusCode(let result):
+            return "Request failed with status code \(result.response.statusCode)."
+        case .unknown(let result):
+            return "Unknown error: \(result)"
+        }
+    }
 }
